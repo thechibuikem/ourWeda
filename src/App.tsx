ea16c3bc39be1319d214e767182de16a07461815
Chukwuemeka David <dev.ataraxia7@gmail.com>
2025-09-15 19:48:45 +0100

    adding preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Preloader from "./pages/Preloader";
 import { WeatherProvider } from "./context/WeatherContext";
-// import NotFound from "./pages/NotFound";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  // Simulate loading for 2 seconds (you can remove timeout later)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <WeatherProvider>
-
-
-      {/* the children of weather context that are using */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </Router>
-    </WeatherProvider>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="*" element={<NotFound />} /> */}
+          </Routes>
+        </Router>
+      )}
+    </WeatherProvider>  
   );
 }
 
