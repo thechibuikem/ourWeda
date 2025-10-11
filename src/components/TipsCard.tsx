0996bf02349e69dd14095cc77f85b67c22b05b7f
Chukwuemeka David <dev.ataraxia7@gmail.com>
2025-10-11 07:07:35 +0100

    added fallback tips

--- src/components/TipsCard.tsx
+++ src/components/TipsCard.tsx
@@ -7,36 +7,50 @@ const MainTips: FC = () => {
   const context = useContext(weatherContext); // create a context instance
   const [tips, setTips] = useState<string[]>([]);
   const [isLoading] = useState(false); // loading state
-  
+
   // Check if context exists
   if (!context) {
     throw new Error("MainTips must be used within WeatherProvider");
   }
 
-  const {recommendations} = context
+  const { recommendations } = context;
 
-  useEffect(()=>{
-if (isLoading) return
+  // function that runs when my tips card component mounts, assigning fallback tips until, Ai generated tips are ready
+  useEffect(() => {
+    if (isLoading) return;
+    else if (recommendations.length < 1) {
+      setTips([
+        "Use natural ventilation instead of AC when possible",
+        "Walk or bike for trips under 2 km",
+        "Unplug idle electronics to cut phantom power",
+        "Choose locally grown seasonal produce today",
+      ]);
+    } 
+    else {
+      setTips(recommendations);
+    }
+  }, [recommendations]);
 
-    setTips(recommendations)
-  }
-,[recommendations])
-
-// where our jsx is returned in
+  // where our jsx is returned in
   return (
     <section className="w-full min:h-[60vh] sm:min-h-[15vh] h-full rounded-sm shadow-md bg-green-600 cursor-arrow hover:-translate-y-1 transition-transform p-4 overflow-y-scroll md:overflow-hidden tips-wrapper flex flex-col gap-y-4">
       {/* generating tips from returned tips from backend  */}
       {tips.length !== 0 ? (
-        tips.map((tip, index) => <p key={index} className="text-white text-md leading-tight">{tip}</p>)
+        tips.map((tip, index) => (
+          <p key={index} className="text-white text-md leading-tight">
+            {tip}
+          </p>
+        ))
       ) : (
         <div className="w-full h-full flex justify-center items-center sm:items-start sm:mt-4">
-          <Preloader width={"4rem"}/>
+          <Preloader width={"4rem"} />
         </div>
       )}
     </section>
   );
 };
 
+// main tips card container
 const TipsCard: FC = () => {
   return (
     <section className="w-full h-full bg-green-800 rounded-xl flex flex-col md:gap-y-8 gap-y-4 items-center pt-4 md:pt-8 md:pb-4 md:px-4 px-4 pb-4 sm:pb-4  shadow-md hover:shadow-sm cursor-pointer hover:-translate-y-1 transition-transform">
